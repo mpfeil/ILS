c333c27ad8a812439c3d9b9afe4c1df3ef359ec0
Matthias Pfeil <matthias.pfeil@wwu.de>
2013-11-15 17:11:49 +0100

    Added shapefile support

--- js/map.js
+++ js/map.js
@@ -5,10 +5,20 @@ var southWest = new L.LatLng(45.120052841530516, -3.7353515625),
 var map = L.map('map',{
 	center: [50.98609893339354, 9.4482421875],
 	zoom: 5,
-	maxBounds: bounds
+	//maxBounds: bounds
 });
 
 L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 }).addTo(map);
+
+var shpfile = new L.Shapefile('DatenMSWissenschaft.zip',{onEachFeature:function(feature, layer) {
+	if (feature.properties) {
+    	layer.bindPopup(Object.keys(feature.properties).map(function(k){
+        	return k + ": " + feature.properties[k] ;
+        }).join("<br />"),{maxHeight:200});
+    }
+}});
+
+shpfile.addTo(map);
